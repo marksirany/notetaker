cdfa775e781e2ec7661efbc70acc2818557b8a3e
Mark Sirany <marksirany@me.com>
2019-12-14 00:51:58 -0600

    not sure

--- server.js
+++ server.js
@@ -1,75 +1,33 @@
+const $noteTitle = $(".note-title");
+const $noteText = $(".note-textarea");
+const $saveNoteBtn = $(".save-note");
+const $newNoteBtn = $(".new-note");
+const $noteList = $(".list-container .list-group");
 // Create a basic server using Express.JS
 // Dependencies
 // =============================================================
+const fs = require("fs");
 const express = require("express");
 const path = require("path");
 const app = express();
+
 const PORT = process.env.PORT || 3000;
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // * Create a few array variables that will hold the data
-const reservations = [{
-    name: "jake",
-    date: "10/10",
-    guests: 2
-}];
-const waitlist = [{
-    name: "kristen",
-    date: "10/10",
-    guests: 2
-}];
-// * Create a set of routes for getting and posting table data
-// Display all reservations
+const getNotes = [];
+const saveNotes = [];
+const deleteNote = [];
+
+// * Create a set of routes for getting and posting  data
+// Display all notes
 app.get("/api/reservations", function (req, res) {
     return res.json(reservations);
 });
+// Display all waitlists
 app.get("/api/waitlist", function (req, res) {
     return res.json(waitlist);
 });
-// Create New Reservation - takes in JSON input
-app.post("/api/reservations", function (req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body parsing middleware
-    const newReservations = req.body;
-    // Using a RegEx Pattern to remove spaces from newCharacter
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    newReservations.routeName = newReservations.name.replace(/\s+/g, "").toLowerCase();
-    console.log(newReservations);
-    reservations.push(newReservations);
-    res.json(newReservations);
-});
-// Create New Reservation - takes in JSON input
-app.post("/api/waitlist", function (req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body parsing middleware
-    const newWaitlist = req.body;
-    // Using a RegEx Pattern to remove spaces from newCharacter
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    newWaitlist.routeName = newWaitlist.name.replace(/\s+/g, "").toLowerCase();
-    console.log(newWaitlist);
-    waitlist.push(newWaitlist);
-    res.json(newWaitlist);
-});
-// * Create a set of routes for displaying the HTML pages
-// Basic route that sends the user first to the AJAX Page
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
-});
-app.get("/home", function (req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
-});
-// * Use jQuery to run AJAX calls to GET and POST data from users to the Express server
-// Starts the server to begin listening
-// =============================================================
-app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
-});
-
-
-//1. CREATE THE FOLLLOWING HTML ROUTES//
-
-//1A. GET `/notes` - Should return the `notes.html` file.//
-router.get('/notes', function (req, res) {
-  res.send('notes');
-})

7c525ded2246dd7fecef8ed3d7c4195be93b98d6
Mark Sirany <marksirany@me.com>
2019-12-14 02:52:46 -0600

    updatd

--- server.js
+++ server.js
@@ -1,11 +1,5 @@
-const $noteTitle = $(".note-title");
-const $noteText = $(".note-textarea");
-const $saveNoteBtn = $(".save-note");
-const $newNoteBtn = $(".new-note");
-const $noteList = $(".list-container .list-group");
 // Create a basic server using Express.JS
-// Dependencies
-// =============================================================
+// Dependencies//
 const fs = require("fs");
 const express = require("express");
 const path = require("path");
@@ -24,10 +18,43 @@ const deleteNote = [];
 
 // * Create a set of routes for getting and posting  data
 // Display all notes
-app.get("/api/reservations", function (req, res) {
-    return res.json(reservations);
+app.get("/api/notes", function (req, res) {
+    return res.json(getNotes);
 });
-// Display all waitlists
-app.get("/api/waitlist", function (req, res) {
-    return res.json(waitlist);
+
+// Create New Note - takes in JSON input
+app.post("/api/notes", function (req, res) {
+    
+
+        // Adds the new note to our array notes
+        getNotes.push(newReservations);
+
+        // Sends the newReservation back to the front-end
+        res.json(newReservations);
+        // If there are already 5 reservations, the reservation request goes to the waitlist
+    } else {
+
+        // req.body hosts is equal to the JSON post sent from the user
+        // This works because of our body parsing middleware
+        const newWaitlist = req.body;
+
+        // Using a RegEx Pattern to remove spaces from newCharacter
+        // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+        newWaitlist.type = "waitlist";
+        // Console logs the newWaitlist information in the server side console.
+        console.log(newWaitlist);
+        res.json(newWaitlist);
+
+        // Adds the new waitlist to our array waitlist
+        waitlist.push(newWaitlist);
+    }
+    // updateTables()
+
+})
+
+
+
+// Starts the server to begin listening//
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
 });

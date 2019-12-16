8e2db8c270f3ed889ad60f2d05944b1a4f71388c
Mark Sirany <marksirany@me.com>
2019-12-15 18:15:31 -0600

    added html routs

--- server.js
+++ server.js
@@ -10,47 +10,60 @@ const PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
-// * Create a few array variables that will hold the data
-const getNotes = [];
-const saveNotes = [];
-const deleteNote = [];
 
-// * Create a set of routes for getting and posting  data
-// Display all notes
-app.get("/api/notes", function (req, res) {
-    return res.json(getNotes);
+//start notes at 0//
+let noteIdMaker = 0;
+
+//----------------------------------------------------------------------------//
+
+
+// * Create a set of routes for getting and posting data//
+
+//1. return note.html file//
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
-// Create New Note - takes in JSON input
-app.post("/api/notes", function (req, res) {
-    
+//2. return the index.html file//
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
 
-        // Adds the new note to our array notes
-        getNotes.push(newReservations);
+//---------------------------------------------------------------------------//
 
-        // Sends the newReservation back to the front-end
-        res.json(newReservations);
-        // If there are already 5 reservations, the reservation request goes to the waitlist
-    } else {
+//The application should have a`db.json` file on the backend that will be used to store and retrieve notes using the`fs` module.//
 
-        // req.body hosts is equal to the JSON post sent from the user
-        // This works because of our body parsing middleware
-        const newWaitlist = req.body;
 
-        // Using a RegEx Pattern to remove spaces from newCharacter
-        // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-        newWaitlist.type = "waitlist";
-        // Console logs the newWaitlist information in the server side console.
-        console.log(newWaitlist);
-        res.json(newWaitlist);
 
-        // Adds the new waitlist to our array waitlist
-        waitlist.push(newWaitlist);
-    }
-    // updateTables()
 
-})
+//---------------------------------------------------------------------------//
+
+//1. GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.//
+
+app.get("/api/notes", function(req, res){
+    fs.readFile('Develop/db/db.json', 'utf8', function(err, data){
+        if(err){
+            console.log(err)
+            return
+        }
+        
+        res.send(data);
+    });
+
+});
+
+
+//2. POST `/api/notes` - Should recieve a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.//
+
+
+
+//3. DELETE `/api/notes/:id` - Should recieve a query paramter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.//
+
+
+
+//---------------------------------------------------------------------------//
 
 
 

4549eab0b3b27f26d34ae8845ebc9e6065a4d4ce
Mark Sirany <marksirany@me.com>
2019-12-18 02:37:36 -0600

    trying to get this towerk

--- server.js
+++ server.js
@@ -1,248 +1,105 @@
-// // Create a basic server using Express.JS
-// // Dependencies//
-// const fs = require("fs");
-// const express = require("express");
-// const path = require("path");
-// const app = express();
-
-// const PORT = process.env.PORT || 3000;
-
-// // Sets up the Express app to handle data parsing
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-// app.use(express.static("public"));
-
-// // * Create a few array variables that will hold the data
-// let noteCreate = 0;
-
-// //----------------------------------------------------------------------------//
-
-// // I. Create a set of routes for getting and posting data//
-
-// //1. return note.html file//
-// app.get("/notes", function (req, res) {
-//     res.sendFile(path.join(__dirname, "/Develop/public/notes.html"));
-// });
-
-// //2. return the index.html file//
-// app.get("/", function (req, res) {
-//     res.sendFile(path.join(__dirname, "/Develop/public/index.html"));
-// });
-
-// //---------------------------------------------------------------------------//
-
-// //The application should have a`db.json` file on the backend that will be used to store and retrieve notes using the`fs` module.//
-
-// //---------------------------------------------------------------------------//
-
-// //II. The following API routes should be created:
-
-// //1. GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.//
-
-// app.get("/api/notes", function (req, res) {
-//     fs.readFile("Develop/db/db.json", "utf8", function (err, data) {
-//         if (err) {
-//             console.log(err);
-//             return;
-//         }
-
-//         res.send(data);
-//     });
-// });
-
-// //2. POST `/api/notes` - Should recieve a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.//
-
-// app.post("/api/notes", function (req, res) {
-//     const newNote = req.body;
-//     let newNotesString = "";
-//     // A new note increases the note id by 1
-//     noteCreate++;
-//     newNote.id = noteCreate;
-
-//     if (newNote) {
-//         console.log(`New note detected: ${newNote}`);
-//         // gets the data from db.json
-//         fs.readFile("Develop/db/db.json", "utf8", function (err, notesString) {
-//             if (err) {
-//                 console.log(`Error occurred during readfile: ${err}`);
-//                 return;
-//             }
-//             console.log(`Current Notes: ${notesString}`);
-//             // If notes already exist, this will be used to add to the current notes.
-//             if (notesString) {
-//                 const notes = JSON.parse(notesString);
-//                 console.log(`Added to already existing note`);
-
-//                 const combinedNotes = [...notes, newNote];
-//                 // Stringifies combined notes
-//                 newNotesString = JSON.stringify(combinedNotes);
-//                 // Writes over previous db.json
-//             } else {
-//                 newNotesString = [JSON.stringify(newNote)];
-//             }
-
-//             fs.writeFile("Develop/db/db.json", newNotesString, function (err) {
-//                 if (err) {
-//                     console.log(err);
-//                     return;
-//                 }
-
-//                 res.send(newNote);
-//             });
-//         });
-//     } else {
-//         res.send("[]");
-//     }
-// });
-
-// //3. DELETE `/api/notes/:id` - Should recieve a query paramter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.//
-
-// //---------------------------------------------------------------------------//
-
-// // Starts the server to begin listening//
-// app.listen(PORT, function () {
-//     console.log("App listening on PORT " + PORT);
-// });
-
-// Create a server using Express.JS
-// Dependencies
-// =============================================================
+// Create a basic server using Express.JS
+// Dependencies//
 const fs = require("fs");
 const express = require("express");
 const path = require("path");
 const app = express();
 
-// This number will be increased by one every time a new note is created.
-let noteIdMaker = 0;
-
-// Set the port. process.env.PORT is for heroku, 3000 is for local testing purposes
 const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
+// * Create a few array variables that will hold the data
+let noteCreate = 0;
 
+//----------------------------------------------------------------------------//
 
-// * Create a set of routes for displaying the HTML pages
-// =========================================
-// * GET `/notes` - Should return the `notes.html` file.
+// I. Create a set of routes for getting and posting data//
+
+//1. return note.html file//
 app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "public/notes.html"));
+    res.sendFile(path.join(__dirname, "/Develop/public/notes.html"));
 });
-// * GET `*` - Should return the `index.html` file
+
+//2. return the index.html file//
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "public/index.html"));
+    res.sendFile(path.join(__dirname, "/Develop/public/index.html"));
 });
 
-// ==========================================
+//---------------------------------------------------------------------------//
 
-// Serve static files
-// -- https://stackoverflow.com/questions/5924072/express-js-cant-get-my-static-files-why
-// -- https://expressjs.com/en/starter/static-files.html
-// =================================================
+//The application should have a`db.json` file on the backend that will be used to store and retrieve notes using the`fs` module.//
 
-app.use(express.static('public'))
+//---------------------------------------------------------------------------//
 
-// =================================================
+//II. The following API routes should be created:
 
-// Create requests for all the requests made by index.js
-// -- This includes get, post, and delete.
-//==================================================
-//   * GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
+//1. GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.//
+
 app.get("/api/notes", function (req, res) {
-    fs.readFile('db/db.json', 'utf8', function (err, data) {
+    fs.readFile("Develop/db/db.json", "utf8", function (err, data) {
         if (err) {
-            console.log(err)
-            return
+            console.log(err);
+            return;
         }
-        // sends data in db to site. file is already a string, so no need for req.json
+
         res.send(data);
     });
-    // console.log(testArray)
 });
-//   * POST `/api/notes` - Should recieve a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
-// Probably a better way to get the new note added in with like lastIndex of so I didn't need to do so much JSON, but this works.
+
+//2. POST `/api/notes` - Should recieve a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.//
+
 app.post("/api/notes", function (req, res) {
     const newNote = req.body;
-    let updatedNotesStringified = "";
-    // A new note is created so increase the note id by 1.
-    noteIdMaker++;
-    newNote.id = noteIdMaker;
-    // If the site is starting with db.json empty it will go to the else statement.
-    // If this is the site adding a new Note it will create a new note.
-    // gets the data from db.json
-    fs.readFile('db/db.json', 'utf8', function (err, notesStringified) {
-        if (err) {
-            console.log(`Error occurred during readfile in post notes: ${err}`)
-            return
-        }
-        console.log(`Current Notes: ${notesStringified}`)
-        // If notes already exist, this will be used to add to the current notes.
-        const notes = JSON.parse(notesStringified)
-        // Parses the data from db.json and stores it in notes.
-        // Combines new note and current notes into a single array by spreading notes into a new array called combined notes and adding newNote.
-        const combinedNotes = [...notes, newNote]
-        // Stringifies combined notes so it can be stored as db.json
-        updatedNotesStringified = JSON.stringify(combinedNotes);
-        // Writes over previous db.json with new db.json data.
-        fs.writeFile('db/db.json', updatedNotesStringified, function (err) {
+    let newNotesString = "";
+    // A new note increases the note id by 1
+    noteCreate++;
+    newNote.id = noteCreate;
+
+    if (newNote) {
+        console.log(`New note detected: ${newNote}`);
+        // gets the data from db.json
+        fs.readFile("Develop/db/db.json", "utf8", function (err, notesString) {
             if (err) {
-                console.log(err)
-                return
+                console.log(`Error occurred during readfile: ${err}`);
+                return;
             }
-            // Homework says to send the new note back to the note.html, but currently unsure what it does with it. Possibly just used for tracking or troubleshooting purposes
-            res.send(newNote);
+            console.log(`Current Notes: ${notesString}`);
+            // If notes already exist, this will be used to add to the current notes.
+            if (notesString) {
+                const notes = JSON.parse(notesString);
+                console.log(`Added to already existing note`);
+
+                const combinedNotes = [...notes, newNote];
+                // Stringifies combined notes
+                newNotesString = JSON.stringify(combinedNotes);
+                // Writes over previous db.json
+            } else {
+                newNotesString = [JSON.stringify(newNote)];
+            }
+
+            fs.writeFile("Develop/db/db.json", newNotesString, function (err) {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+
+                res.send(newNote);
+            });
         });
-    });
-})
+    } else {
+        res.send("[]");
+    }
+});
 
-//   * DELETE `/api/notes/:id` - Should recieve a query paramter containing the id of a note to delete.
-// :id stores the value in the path in the property of req.params. This is feature of express ... I think
-app.delete('/api/notes/:id', function (req, res) {
-    // Stores the of the note that should be deleted.
-    // -- id is a string so it needs to be converted to a integer
-    const noteToDeleteId = parseInt(req.params.id);
-    console.log(noteToDeleteId)
-    // Sends a response back to the page that the delete request was completed
-    let notesStringified = "";
+//3. DELETE `/api/notes/:id` - Should recieve a query paramter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.//
 
-    // gets the data from db.json
-    fs.readFile('db/db.json', 'utf8', function (err, notesStringified) {
-        if (err) {
-            console.log(err)
-            return
-        }
-        // Parses the data from db.json and stores it in notes.
-        const notes = JSON.parse(notesStringified);
-        console.log(notes);
-        // Combines new note and current notes into a single array by spreading notes into a new array called combined notes and adding newNote.
-        for (let i = 0; i < notes.length; i++) {
-            console.log("notes.length", notes.length)
-            console.log("notes i", notes[i])
-            console.log("notes id", notes[i].id)
-            if (notes[i].id === noteToDeleteId) {
-                notes.splice(i, 1);
-                notesStringified = JSON.stringify(notes);
-                console.log("note deleted.", notesStringified)
-            }
-        }
+//---------------------------------------------------------------------------//
 
-
-        // Writes over previous db.json with new db.json data.
-        fs.writeFile('db/db.json', notesStringified, function (err) {
-            if (err) {
-                console.log(err)
-                return
-            }
-        });
-        res.send('Got a DELETE request at /user')
-    });
-})
-
-
-// Starts the server to begin listening
-// =============================================================
+// Starts the server to begin listening//
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
